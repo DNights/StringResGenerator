163133be7324bc73e1cc6de53be9fa0b448fdaff
hyun dong han <hyundh@deali.net>
2021-12-22 15:48:43 +0900

    edit shell script

--- android.sh
+++ android.sh
@@ -1,2 +1,4 @@
-python ./quickstart.py
-python ./xlsx2string_res_for_AOS.py
+python3 ./download_google_drive.py 
+python3 ./xlsx2string_res_for_AOS.py
+# python ./download_google_drive.py 
+# python ./xlsx2string_res_for_AOS.py
--- ios.sh
+++ ios.sh
@@ -1,2 +1,4 @@
-python ./quickstart.py
-python ./xlsx2string_res_for_IOS.py
+python3 ./download_google_drive.py 
+python3 ./xlsx2string_res_for_IOS.py
+# python ./download_google_drive.py 
+# python ./xlsx2string_res_for_IOS.py
